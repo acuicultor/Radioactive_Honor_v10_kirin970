7c199e0326ea6aff68919e4f730777c0a60635be
acuicultor <ramoncuenca4@gmail.com>
2018-04-27 21:34:20 +0200

    Build.sh: switch to ubernaro 4.9 TC

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export PATH=$PATH:/home/acuicultor/Android/toolchains/aarch64-linux-android-4.9/bin
+export PATH=$PATH:/home/acuicultor/Android/toolchains/ubernaro-aarch64-linux-android-4.9/bin
 export CROSS_COMPILE=aarch64-linux-android-
 mkdir out
 make ARCH=arm64 O=out radioactive_defconfig

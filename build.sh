d34ac40211a973cab4c71cc3d659b0fe769c28f3
acuicultor <ramoncuenca4@gmail.com>
2018-04-28 21:44:54 +0200

    Build.sh: add build user and build host

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 export PATH=$PATH:/home/acuicultor/Android/toolchains/ubernaro-aarch64-linux-android-4.9/bin
 export CROSS_COMPILE=aarch64-linux-android-
+export KBUILD_BUILD_USER=acuicultor
+export KBUILD_BUILD_HOST=NuclearPowerPlant
 mkdir out
 make ARCH=arm64 O=out radioactive_defconfig
 make ARCH=arm64 O=out -j5

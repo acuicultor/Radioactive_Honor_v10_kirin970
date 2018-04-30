68704757e49770e207bc92a24dccba456978d7e6
acuicultor <ramoncuenca4@gmail.com>
2018-04-30 10:36:52 +0200

    Build.sh: get ready for public release
    *also change default tcp congestion algorithm to sociopath

--- build.sh
+++ build.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
-export PATH=$PATH:/home/acuicultor/Android/toolchains/ubernaro-aarch64-linux-android-4.9/bin
-export CROSS_COMPILE=aarch64-linux-android-
+
+# Radioactive Kernel Details
+KERNEL_NAME="~Radioactive-"
+INCREMENTAL_VERSION="V1.0.0"
+export LOCALVERSION="${KERNEL_NAME}-${INCREMENTAL_VERSION}"
 export KBUILD_BUILD_USER=acuicultor
 export KBUILD_BUILD_HOST=NuclearPowerPlant
+export PATH=$PATH:/home/acuicultor/Android/toolchains/ubernaro-aarch64-linux-android-4.9/bin
+export CROSS_COMPILE=aarch64-linux-android-
 mkdir out
 make ARCH=arm64 O=out radioactive_defconfig
 make ARCH=arm64 O=out -j5

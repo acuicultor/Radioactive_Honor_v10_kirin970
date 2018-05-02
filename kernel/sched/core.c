kerneltoast/android_kernel_oneplus_msm8996

kthread: Force all non-percpu kthreads onto the power cluster
Force all kthreads that don't need to be on a specific CPU onto the power
cluster in order to save power. This overrides anyone (user-land and
kernel-land) who wants to change the CPU affinity of a non-percpu kthread.

Signed-off-by: Sultanxda <sultanxda@gmail.com>

kernel/sched/core.c
@@ -8209,8 +8209,22 @@ static struct rq *move_queued_task(struct task_struct *p, int new_cpu)
 	return rq;
 }
 
+static const struct cpumask *adjust_cpumask(const struct task_struct *p,
+	const struct cpumask *old_mask)
+{
+	static const unsigned long allowed_cpus = 0x3;
+
+	if (!(p->flags & PF_KTHREAD) || p->kthread_per_cpu)
+		return old_mask;
+
+	/* Force as many kthreads as possible to run on the little cluster */
+	return to_cpumask(&allowed_cpus);
+}
+
 void do_set_cpus_allowed(struct task_struct *p, const struct cpumask *new_mask)
 {
+	new_mask = adjust_cpumask(p, new_mask);
+
 	if (p->sched_class && p->sched_class->set_cpus_allowed)
 		p->sched_class->set_cpus_allowed(p, new_mask);
 
@@ -8248,6 +8262,7 @@ int set_cpus_allowed_ptr(struct task_struct *p, const struct cpumask *new_mask)
 	unsigned int dest_cpu;
 	int ret = 0;
 
+	new_mask = adjust_cpumask(p, new_mask);
 	rq = task_rq_lock(p, &flags);
 
 	if (cpumask_equal(&p->cpus_allowed, new_mask))

acuicultor/Radioactive_Honor_v10_kirin970

Signed-off-by: UchihaDev <ajit4guraya@gmail.com>

kernel/sched/core.c
@@ -1189,6 +1189,18 @@ void set_cpus_allowed_common(struct task_struct *p, const struct cpumask *new_ma
 	p->nr_cpus_allowed = cpumask_weight(new_mask);
 }
 
+static const struct cpumask *adjust_cpumask(const struct task_struct *p,
+	const struct cpumask *old_mask)
+{
+	static const unsigned long allowed_cpus = 0xf;
+
+	if (!(p->flags & PF_KTHREAD) || p->kthread_per_cpu)
+		return old_mask;
+
+	/* Force as many kthreads as possible to run on the little cluster */
+	return to_cpumask(&allowed_cpus);
+}
+
 void do_set_cpus_allowed(struct task_struct *p, const struct cpumask *new_mask)
 {
 	struct rq *rq = task_rq(p);
@@ -1196,6 +1208,7 @@ void do_set_cpus_allowed(struct task_struct *p, const struct cpumask *new_mask)
 
 	lockdep_assert_held(&p->pi_lock);
 
+	new_mask = adjust_cpumask(p, new_mask);
 	queued = task_on_rq_queued(p);
 	running = task_current(rq, p);
 
@@ -5418,6 +5431,7 @@ void sched_setnuma(struct task_struct *p, int nid)
 	unsigned long flags;
 	bool queued, running;
 
+	new_mask = adjust_cpumask(p, new_mask);
 	rq = task_rq_lock(p, &flags);
 	queued = task_on_rq_queued(p);
 	running = task_current(rq, p);